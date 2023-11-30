4bb748f79cdce46e1d91caf90b9f4e37e95c5791
Jeon-YJ1004 <dudwn5374@naver.com>
2023-11-30 22:34:25 +0900

    Revert "fix: change login url in userApi"
    This reverts commit 6accc630d3a84467863ce159308d5b181c1bb103.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { history } from './redux/configStore';
 import { HistoryRouter as Router } from "redux-first-history/rr6";
 
+import axios from "axios";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
@@ -13,6 +14,8 @@ import ModalsProvider from './components/modal/ModalsProvider';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export let persistor = persistStore(store);
+axios.defaults.withCredentials = true; // withCredentials 전역 설정
+
 
 ReactModal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
